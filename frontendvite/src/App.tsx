adc0a8a5a8382563e36eaf5a4ff4527cd5befbf1
cahyono muslim sidiq <cdq.cahyono@gmail.com>
2024-07-08 16:30:27 +0700

    add vite

---
+++ frontendvite/src/App.tsx
@@ -0,0 +1,25 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WEBSITE UNDER CONTRUCTION</h1>
+      <div className="card">
+        <p>Please wait until production ready ....</p>
+      </div>
+      <p className="read-the-docs">Welcome in network store</p>
+    </>
+  );
+}
+
+export default App;
