f196365b2911c003f0e2ebcf62073606ac87939b
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-07-11 12:33:17 +0700

    modified app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WEBSITE UNDER CONTRUCTION</h1>
-      <div className="card">
-        <p>Welcome in Network Strore Kebumen</p>
-      </div>
-      <p className="read-the-docs">Please wait until production ready ....</p>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<h1>WELCOME IN NETWORKSTORE</h1>
+				<div className="card">
+					<p>Network Store Kebumen</p>
+				</div>
+				<p className="read-the-docs"></p>
+			</div>
+		</>
+	);
 }
 
 export default App;
