c5253d0315cecda98f6c73a819c99955a86b671a
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-07-17 11:15:35 +0700

    modif App

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -20,36 +20,27 @@ export async function loader() {
 export function App() {
 	const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
 	return (
-		<div>
-			<div>
-				<div className=" items-center justify-center space-x-2 my-6 mx4  grid grid-cols-3 gap-3">
-					{products.map((product) => {
-						return (
-							<Card
-								key={product.id}
-								href="/product/"
-								className="w-[303px] h-[404px] bg-transparent"
-							>
-								<div>
-									<img
-										src={product.imageUrl}
-										alt={product.name}
-									/>
-								</div>
-								<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-									{product.name}
-								</h5>
-								<p className="font-normal text-gray-700 dark:text-gray-400">
-									{product.description}
-								</p>
-								<p className="text-center font-bold">
-									{product.price}
-								</p>
-							</Card>
-						);
-					})}
-				</div>
-			</div>
+		<div className=" items-center justify-center space-x-2 my-6 mx4  grid grid-cols-3">
+			{products.map((product) => {
+				return (
+					<Card
+						key={product.id}
+						href="/product/"
+						className="w-[303px] h-[404px] bg-transparent"
+					>
+						<div>
+							<img src={product.imageUrl} alt={product.name} />
+						</div>
+						<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{product.name}
+						</h5>
+						<p className="font-normal text-gray-700 dark:text-gray-400">
+							{product.description}
+						</p>
+						<p className="text-center font-bold">{product.price}</p>
+					</Card>
+				);
+			})}
 		</div>
 	);
 }
