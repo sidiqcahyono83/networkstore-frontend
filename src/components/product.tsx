99a9a6d4f2001cd4e90003aa1531b61c55fef645
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-07-17 10:46:01 +0700

    add Products fecth

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,30 +1,55 @@
 import { Card } from "flowbite-react";
+import type { Product } from "../types/product";
+import { useLoaderData } from "react-router-dom";
+
+export async function loader() {
+	try {
+		const response = await fetch(
+			`${import.meta.env.VITE_BACKEND_API_URL}/products`
+		);
+		const responseJSON = await response.json();
+
+		const products: Product[] = responseJSON.data;
+
+		// console.log(products);
+		return { products };
+	} catch (error) {
+		return { products: [] };
+	}
+}
 
 export function Product() {
-  return (
-    <div>
-      <div className="max-w-full mx-auto items-center h-[1203px]">
-        <div className="flex items-center justify-center space-x-2 my-6 mx4">
-          <ul>
-            <li>
-              <Card href="#" className="w-[303px] h-[404px] bg-transparent">
-                <div>
-                  <img src="" alt="CCR3" />
-                </div>
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Routerboard CCR2004-1G-12S+2XS
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                  The Connectivity Router - your best companion when it comes to
-                  SFP, SFP+ and SFP28 management! 1, 10 and 25 Gbps ports in a
-                  single device to make your life easier
-                </p>
-                <p className="text-center font-bold">Rp. 8.240.000,00</p>
-              </Card>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+	const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+
+	return (
+		<div>
+			<div className=" items-center justify-center space-x-2 mx-6 my-6  grid grid-cols-3 gap-3">
+				{products.map((product) => {
+					return (
+						<Card
+							key={product.id}
+							href="#"
+							className="w-[303px] h-[404px] bg-transparent"
+						>
+							<div>
+								<img
+									src={product.imageUrl}
+									alt={product.name}
+								/>
+							</div>
+							<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+								{product.name}
+							</h5>
+							<p className="font-normal text-gray-700 dark:text-gray-400">
+								{product.description}
+							</p>
+							<p className="text-center font-bold">
+								{product.price}
+							</p>
+						</Card>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,43 +5,44 @@ import React from "react";
 import "./index.css";
 import { Layout } from "./routes/Layout";
 import { App, loader as appLoader } from "./routes/App";
-import { Product } from "./components/product";
+import { Product, loader as productLoader } from "./components/product";
 import { Chart } from "./components/chart";
 import { Daftar } from "./components/daftar";
 import { Login } from "./components/login";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <App />,
-        loader: appLoader,
-      },
-      {
-        path: "/product",
-        element: <Product />,
-      },
-      {
-        path: "/chart",
-        element: <Chart />,
-      },
-      {
-        path: "/daftar",
-        element: <Daftar />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <App />,
+				loader: appLoader,
+			},
+			{
+				path: "/product",
+				element: <Product />,
+				loader: productLoader,
+			},
+			{
+				path: "/chart",
+				element: <Chart />,
+			},
+			{
+				path: "/daftar",
+				element: <Daftar />,
+			},
+			{
+				path: "/login",
+				element: <Login />,
+			},
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
