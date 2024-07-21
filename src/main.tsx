e830a0e22fb9e4143f3782c5f650d7645d2ac0a8
M Haidar Hanif <git@mhaidarhanif.com>
2024-07-21 13:34:37 +0700

    fix: the real product route

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import "./index.css";
 import { Layout } from "./routes/Layout";
 import { App, loader as appLoader } from "./routes/App";
 import { Products, loader as productsLoader } from "./routes/products";
-import { Product, loader as productLoader } from "./routes/product";
-import { Cart, loader as cartLoader } from "./routes/cart";
+import { ProductRoute, loader as productLoader } from "./routes/product";
 
 import { Register } from "./routes/register";
 import { Login } from "./routes/login";
@@ -15,52 +14,47 @@ import { Cartitem } from "./routes/cartitem";
 import { Component } from "./routes/acordion";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				path: "/",
-				element: <App />,
-				loader: appLoader,
-			},
-			{
-				path: "/products",
-				element: <Products />,
-				loader: productsLoader,
-			},
-			{
-				path: "/products/:productId",
-				element: <Product />,
-				loader: productLoader,
-			},
-			{
-				path: "/cart",
-				element: <Cartitem />,
-			},
-			{
-				path: "/cart/:productId",
-				element: <Cart />,
-				loader: cartLoader,
-			},
-			{
-				path: "/daftar",
-				element: <Register />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/component",
-				element: <Component />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+        loader: appLoader,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+        loader: productsLoader,
+      },
+      {
+        path: "/products/:productId",
+        element: <ProductRoute />,
+        loader: productLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cartitem />,
+      },
+      {
+        path: "/daftar",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/component",
+        element: <Component />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
--- src/routes/product.tsx
+++ src/routes/product.tsx
@@ -1,63 +1,100 @@
-import { type LoaderFunctionArgs, useLoaderData } from "react-router-dom";
+import { Form, type LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 
 import type { Product } from "../types/product";
-import { Card } from "flowbite-react";
+import { Button } from "flowbite-react";
 import { formatIDR } from "../lib/formatCurency";
-import { FaCartShopping } from "react-icons/fa6";
+import { useState } from "react";
+import { Input } from "@headlessui/react";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-	const productId = String(params.productId);
+  const productId = String(params.productId);
 
-	try {
-		const response = await fetch(
-			`${import.meta.env.VITE_BACKEND_API_URL}/products/${productId}`
-		);
-		const responseJSON = await response.json();
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_BACKEND_API_URL}/products/${productId}`
+    );
+    const responseJSON = await response.json();
 
-		const product: Product = responseJSON.data;
+    const product: Product = responseJSON.data;
 
-		console.log(product);
-		return { product };
-	} catch (error) {
-		return { product: null };
-	}
+    console.log(product);
+    return { product };
+  } catch (error) {
+    return { product: null };
+  }
 }
 
-export function Product() {
-	const { product } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
-	if (!product) {
-		return "Product not found";
-	}
+export function ProductRoute() {
+  const { product } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const [quantity, setQuantity] = useState<number>(1);
 
-	return (
-		<div className="mx-auto">
-			<div className="items-center justify-center">
-				<Card
-					key={product.id}
-					className="max-w-2lg bg-transparent mx-auto"
-				>
-					<div>
-						<img src={product.imageUrl} alt={product.name} />
-					</div>
-					<h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-						{product.name}
-					</h5>
-					<p className="font-normal text-gray-700 dark:text-gray-400">
-						{product.description}
-					</p>
-					<p className="text-center font-bold">
-						{formatIDR(product.price)}
-					</p>
-					<div className="flex justify-between">
-						<a href={`/cart/${product.id}`}>
-							<FaCartShopping />
-						</a>
-						<button>
-							<FaCartShopping />
-						</button>
-					</div>
-				</Card>
-			</div>
-		</div>
-	);
+  if (!product) {
+    return "Product not found";
+  }
+
+  const handlePlus = () => {
+    setQuantity((quantity) => quantity + 1);
+  };
+
+  const handleMinus = () => {
+    setQuantity((quantity) => quantity - 1);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuantity(parseInt(e.target.value));
+  };
+
+  return (
+    <div className="mx-auto ">
+      <div className="items-center justify-center mx-auto">
+        <Form>
+          <div key={product.id} className="max-w-2lg bg-transparent mx-auto">
+            <div>
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mx-6">
+              {product.name}
+            </h5>
+
+            <p className="font-normal text-gray-700 dark:text-gray-400 mx-6">
+              {product.description}
+            </p>
+            <p className="text-center font-bold mt-6">
+              {formatIDR(product.price)}
+            </p>
+            <div className="flex flex-wrap gap-2 m-6">
+              <div className="mt-4 flex w-full items-center space-x-2 justify-center">
+                <Button
+                  type="button"
+                  onClick={handleMinus}
+                  disabled={quantity === 1}
+                >
+                  -
+                </Button>
+                <Input
+                  id="quantity"
+                  name="quantity"
+                  className="text-center"
+                  type="number"
+                  min={1}
+                  value={quantity}
+                  onChange={handleChange}
+                  required
+                />
+                <Button type="button" onClick={handlePlus}>
+                  +
+                </Button>
+              </div>
+              <Button
+                type="submit"
+                className="mt-8 rounded-md h-12 w-full uppercase items-center "
+              >
+                Add to cart
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
