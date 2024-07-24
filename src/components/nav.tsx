d5adf4386d4ccb5c8b97437080f39c46ca0edcad
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-07-24 20:06:19 +0700

    modif nav

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,17 +2,19 @@ import {
 	Disclosure,
 	DisclosureButton,
 	DisclosurePanel,
-	Menu,
-	MenuButton,
-	MenuItem,
-	MenuItems,
 } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { IoPerson } from "react-icons/io5";
-import { IoCaretForwardCircleSharp } from "react-icons/io5";
+
+// Menu,
+// MenuButton,
+// MenuItem,
+// MenuItems,
+//  BellIcon,
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { IoPerson } from "react-icons/io5";
+// import { IoCaretForwardCircleSharp } from "react-icons/io5";
 
 const navigation = [
-	{ name: "Home", href: "/", current: true },
+	// { name: "Home", href: "/", current: true },
 	{ name: "Products", href: "/products", current: false },
 	{ name: "Cart", href: "/cart", current: false },
 ];
@@ -43,11 +45,13 @@ export default function Navbar() {
 					</div>
 					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 						<div className="flex flex-shrink-0 items-center">
-							<img
-								alt="Your Company"
-								src="/ns.png"
-								className="h-8 w-auto"
-							/>
+							<a href="/">
+								<img
+									alt="Your Company"
+									src="/ns.png"
+									className="h-8 w-auto"
+								/>
+							</a>
 						</div>
 						<div className="hidden sm:ml-6 sm:block">
 							<div className="flex space-x-4">
@@ -72,17 +76,17 @@ export default function Navbar() {
 						</div>
 					</div>
 					<div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-						<button
+						{/* <button
 							type="button"
 							className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 						>
 							<span className="absolute -inset-1.5" />
 							<span className="sr-only">View notifications</span>
 							<BellIcon aria-hidden="true" className="h-6 w-6" />
-						</button>
+						</button> */}
 
 						{/* Profile dropdown */}
-						<Menu as="div" className="relative ml-3">
+						{/* <Menu as="div" className="relative ml-3">
 							<div>
 								<MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 									<span className="absolute -inset-1.5" />
@@ -123,7 +127,7 @@ export default function Navbar() {
 									</a>
 								</MenuItem>
 							</MenuItems>
-						</Menu>
+						</Menu> */}
 					</div>
 				</div>
 			</div>
