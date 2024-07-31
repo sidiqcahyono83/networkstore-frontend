dea267fb20b2d96efe45462e350955fad56d7fe5
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-07-31 11:09:32 +0700

    feat: cartitem

--- src/routes/cartitem.tsx
+++ src/routes/cartitem.tsx
@@ -27,48 +27,9 @@ export const Cartitem = () => {
 	const { cart } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
 
 	return (
-		<div className="w-full my-6 mx-auto">
-			<div className="w-100%">
-				<div className="mb-4 flex items-center justify-between">
-					<h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-						Latest Cart
-					</h5>
-					<a
-						href="#"
-						className="text-sm font-medium text-cyan-600 hover:underline dark:text-cyan-500"
-					>
-						View all
-					</a>
-				</div>
-				<div className="w-full">
-					<ul className="divide-y divide-gray-200 dark:divide-gray-700">
-						<li className="py-3 sm:py-4">
-							<div className="flex items-center space-x-4">
-								<div className="shrink-0">
-									<img
-										alt="CCR"
-										height="95"
-										src=""
-										width="95"
-										className="rounded-full"
-									/>
-								</div>
-								<pre>{JSON.stringify(cart, null, 2)}</pre>
-
-								<div className="min-w-0 flex-1">
-									<p className="truncate text-sm font-medium text-gray-900 dark:text-white"></p>
-
-									<p className="truncate text-sm text-gray-500 dark:text-gray-400"></p>
-								</div>
-								<div className="min-w-0 flex-1">
-									<p className="truncate text-sm text-gray-500 dark:text-gray-400"></p>
-								</div>
-								<div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"></div>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
+		<>
+			<h2>Shopping Cart</h2>
+			<pre>{JSON.stringify(cart, null, 2)}</pre>
+		</>
 	);
 };
