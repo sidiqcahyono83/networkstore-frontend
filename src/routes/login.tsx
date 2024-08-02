a8f73a89aedc65dde57be84b6d83af0c6ffe8beb
M Haidar Hanif <git@mhaidarhanif.com>
2024-08-02 22:52:24 +0700

    fix: login flow

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -3,82 +3,82 @@ import { ActionFunctionArgs, Form, redirect } from "react-router-dom";
 import { authCookie } from "../modules/auth";
 
 type LoginResponse = {
-	message: string;
-	token: string;
+  message: string;
+  token: string;
 };
 
 // Fungsi untuk menangani aksi form submission
 export async function action({ request }: ActionFunctionArgs) {
-	const formData = await request.formData();
+  const formData = await request.formData();
 
-	const userData = {
-		username: formData.get("username")?.toString(),
-		password: formData.get("password")?.toString(),
-	};
+  const userData = {
+    username: formData.get("username")?.toString(),
+    password: formData.get("password")?.toString(),
+  };
 
-	const response = await fetch(
-		`${import.meta.env.VITE_BACKEND_API_URL}/auth/login`,
-		{
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(userData),
-		}
-	);
-	console.log(userData);
-	const loginResponse: LoginResponse = await response.json();
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    }
+  );
 
-	if (!loginResponse) {
-		return null;
-	}
+  const loginResponse: LoginResponse = await response.json();
 
-	//mengambil token
-	const token = loginResponse.token;
+  if (!loginResponse.token) {
+    return null;
+  }
 
-	//Set token di cookie
-	authCookie.set("token", token);
+  //mengambil token
+  const token = loginResponse.token;
 
-	return redirect("/products");
+  //Set token di cookie
+  authCookie.set("token", token);
+
+  return redirect("/products");
 }
 
 export function Login() {
-	return (
-		<div className="h-[1280px] mx-8 my-10 justify-center">
-			<Form
-				method="post"
-				className="flex justify-center max-w-md flex-col gap-4"
-			>
-				<div className="justify-center">
-					<div className="mb-2 block">
-						<Label htmlFor="username" value="username" />
-					</div>
-					<TextInput
-						id="username"
-						type="username"
-						name="username"
-						placeholder="username"
-						required
-						shadow
-					/>
-				</div>
+  return (
+    <div className="h-[1280px] mx-8 my-10 justify-center">
+      <Form
+        method="post"
+        className="flex justify-center max-w-md flex-col gap-4"
+      >
+        <div className="justify-center">
+          <div className="mb-2 block">
+            <Label htmlFor="username" value="username" />
+          </div>
+          <TextInput
+            id="username"
+            type="username"
+            name="username"
+            placeholder="username"
+            required
+            shadow
+          />
+        </div>
 
-				<div className="justify-center">
-					<div className="mb-2 block">
-						<Label htmlFor="password" value="password" />
-					</div>
-					<TextInput
-						id="password"
-						type="password"
-						name="password"
-						placeholder="password"
-						required
-						shadow
-					/>
-				</div>
+        <div className="justify-center">
+          <div className="mb-2 block">
+            <Label htmlFor="password" value="password" />
+          </div>
+          <TextInput
+            id="password"
+            type="password"
+            name="password"
+            placeholder="password"
+            required
+            shadow
+          />
+        </div>
 
-				<Button type="submit">Login</Button>
-			</Form>
-		</div>
-	);
+        <Button type="submit">Login</Button>
+      </Form>
+    </div>
+  );
 }
--- src/routes/products.tsx
+++ src/routes/products.tsx
@@ -4,55 +4,51 @@ import { useLoaderData } from "react-router-dom";
 import { formatIDR } from "../lib/formatCurency";
 
 export async function loader() {
-	try {
-		const response = await fetch(
-			`${import.meta.env.VITE_BACKEND_API_URL}/products`
-		);
-		const responseJSON = await response.json();
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_BACKEND_API_URL}/products`
+    );
+    const responseJSON = await response.json();
 
-		const products: Product[] = responseJSON.data;
+    const products: Product[] = responseJSON.data;
 
-		// console.log(products);
-		return { products };
-	} catch (error) {
-		return { products: [] };
-	}
+    // console.log(products);
+    return { products };
+  } catch (error) {
+    return { products: [] };
+  }
 }
 
 export function Products() {
-	const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
 
-	return (
-		<div className="mx-auto">
-			<div className=" items-center justify-center grid sm:grid-cols-3 gap-4 my-4 mx-auto">
-				{products.map((product) => {
-					return (
-						<div className="grid grid-flow-col">
-							<Card
-								key={product.id}
-								href={`/products/${product.id}`}
-								className="w-[303px] h-[404px] bg-transparent mx-auto"
-							>
-								<div className="max-h-[200px] max-w-[150px]  mx-auto">
-									<img
-										src={product.imageUrl}
-										alt={product.name}
-									/>
-								</div>
-								<h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-									{product.name}
-								</h5>
-								<p className="font-normal text-gray-700 dark:text-gray-400">
-									{product.description}
-								</p>
-								<p className="text-center font-bold">
-									{formatIDR(product.price)}
-								</p>
-							</Card>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="mx-auto">
+      <div className=" items-center justify-center grid sm:grid-cols-3 gap-4 my-4 mx-auto">
+        {products.map((product) => {
+          return (
+            <div key={product.id} className="grid grid-flow-col">
+              <Card
+                href={`/products/${product.id}`}
+                className="w-[303px] h-[404px] bg-transparent mx-auto"
+              >
+                <div className="max-h-[200px] max-w-[150px]  mx-auto">
+                  <img src={product.imageUrl} alt={product.name} />
+                </div>
+                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {product.name}
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {product.description}
+                </p>
+                <p className="text-center font-bold">
+                  {formatIDR(product.price)}
+                </p>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
