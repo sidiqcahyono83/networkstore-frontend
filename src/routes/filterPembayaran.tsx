4d68b9a11f1c86849fc7dd6d9f3daa521cfed472
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-10-20 20:50:20 +0700

    feat:fix sudah bayar

--- src/routes/filterPembayaran.tsx
+++ src/routes/filterPembayaran.tsx
@@ -126,57 +126,66 @@ export function PembayaranFilter() {
       </h2>
 
       <form onSubmit={handleFilter}>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Bulan:
-          </label>
-          <select
-            value={month}
-            onChange={(e) => setMonth(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        <div className="flex grid-cols-3 items-center gap-4 ">
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Bulan:
+            </label>
+            <select
+              value={month}
+              onChange={(e) => setMonth(e.target.value)}
+              className="mt-1 block w-56 border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            >
+              <option value="">Pilih Bulan</option>
+              {months.map((month) => (
+                <option key={month.value} value={month.value}>
+                  {month.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Metode:
+            </label>
+            <select
+              value={metode}
+              onChange={(e) => setMetode(e.target.value)}
+              className="mt-1 block w-56 border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            >
+              <option value="">Pilih Metode Pembayaran</option>
+              {metodeOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700">
+              Admin Username:
+            </label>
+            <select
+              value={adminUsername}
+              onChange={(e) => setAdminUsername(e.target.value)}
+              className="mt-1 block w-56 border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            >
+              <option value="">Pilih Admin</option>
+              {adminOptions.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="inline-flex justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            <option value="">Pilih Bulan</option>
-            {months.map((month) => (
-              <option key={month.value} value={month.value}>
-                {month.label}
-              </option>
-            ))}
-          </select>
+            Filter
+          </button>
         </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Metode:
-          </label>
-          <select
-            value={metode}
-            onChange={(e) => setMetode(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          >
-            <option value="">Pilih Metode Pembayaran</option>
-            {metodeOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Admin Username:
-          </label>
-          <select
-            value={adminUsername}
-            onChange={(e) => setAdminUsername(e.target.value)}
-            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          >
-            <option value="">Pilih Admin</option>
-            {adminOptions.map((option) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
-        </div>
+
         {/* <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700">
             Tanggal:
@@ -188,12 +197,6 @@ export function PembayaranFilter() {
             className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
           />
         </div> */}
-        <button
-          type="submit"
-          className="inline-flex justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Filter
-        </button>
       </form>
 
       {filteredData.length > 0 ? (
